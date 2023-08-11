f684d437a0d8e9428a4ac199a143a29e32a5632c
abdulla <tallaey445@gmail.com>
2023-08-11 22:42:25 +0000

    re

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,6 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div 
     import sys
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    print("{} argument".format(len(sys.argv))
+    for i in range(1, len(sys.argv)):
+      print("{}: {}".format(str(sys.argv[i]))
