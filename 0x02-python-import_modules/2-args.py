42f5171f6042921611762c98ae581d1afd1f884d
abdulla <tallaey445@gmail.com>
2023-08-11 22:53:34 +0000

    op

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div 
     import sys
 
-    print("{} argument".format(len(sys.argv))
-    for i in range(1, len(sys.argv)):
-      print("{}: {}".format(str(sys.argv[i]))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
